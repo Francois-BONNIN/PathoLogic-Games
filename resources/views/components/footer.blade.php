2ef0c3bc889da296e08c9c23f31daeb2c405528d
Francois BONNIN <francois.bonnin@ynov.com>
2022-03-30 16:51:32 +0200

    Footer init

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,30 @@
-<footer>
+<footer class="footer p-10 text-base-content">
+    <div>
+        <p>ACME Industries Ltd.<br>Providing reliable tech since 1992</p>
+    </div>
+    <div>
+        <span class="footer-title">Services</span>
+        <a class="link link-hover">Branding</a>
+        <a class="link link-hover">Design</a>
+        <a class="link link-hover">Marketing</a>
+        <a class="link link-hover">Advertisement</a>
+    </div>
+    <div>
+        <span class="footer-title">Legal</span>
+        <a class="link link-hover">Terms of use</a>
+        <a class="link link-hover">Privacy policy</a>
+        <a class="link link-hover">Cookie policy</a>
+    </div>
+    <div>
+        <span class="footer-title">Newsletter</span>
+        <div class="form-control w-80">
+            <label class="label">
+                <span class="label-text">Enter your email address</span>
+            </label>
+            <div class="relative">
+                <input type="text" placeholder="username@site.com" class="input input-bordered w-full pr-16">
+                <button class="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+            </div>
+        </div>
 
 </footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
     <div class="hero flex flex-col lg:flex-row">
         <div class="heroText flex flex-col">
             <h1>Bienvenue au studio</h1>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+            <p>Pathologic Games est un studio de développement indépendant créé en 2021 et basé à Toulouse, en France.
+                Le studio regroupe une dizaine de personnes dont des artistes, des créatifs et des développeurs. Notre ambition est de proposer du divertissement vidéoludique mélangeant scénario atypique et gameplay original.
+            </p>
             <a href="" class="btn btn-sm btn-primary ">Découvrir</a>
         </div>
         <div class="heroMenu flex flex-col md:flex-row lg:flex-row">
@@ -13,7 +15,7 @@
                 <div class="hero-items">
                     <img src="{{asset('img/home/Joystick.svg')}}">
                     <h3>Jeux</h3>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <p>Pathologic Games est un studio junior créé en 2021. Nous travaillons actuellement sur notre premier jeux, Medium’s Hat, un jeu vidéo 2D de type point'n'clik mêlant habilement gameplay et narration. Le jeu mettra le joueur dans la peau de Georges, un medium capable de parler avec les victimes d'un tueur en série en portant le chapeau melon de ce dernier.</p>
                     <a class="btn btn-primary" href="{{ route('mediums-hat') }}">Découvrir</a>
                 </div>
             </div>
@@ -22,7 +24,7 @@
                 <div class="hero-items">
                     <img src="{{asset('img/home/Search.svg')}}">
                     <h3>A propos</h3>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <p>Créé dans le but de proposer des expériences vidéoludiques uniques et impactantes, Pathologic Games donne une grande importance à l’écriture de ses projets ainsi qu’à la conception du gameplay. L’équipe junior, dynamique et avertie, mène sa réflexion dans cet unique objectif.</p>
                     <a class="btn btn-primary" href="{{ route('aboutus') }}">Découvrir</a>
                 </div>
             </div>
@@ -31,7 +33,7 @@
                 <div class="hero-items">
                     <img src="{{asset('img/home/Team.svg')}}">
                     <h3>L'équipe</h3>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <p>Composée de développeurs, d’artistes et de webdesigners, l’équipe de Pathologic Games est variée, jeune et dynamique. Elle n’a qu’un seul objectif commun : créer des jeux uniques et authentiques afin que les joueurs, amateurs ou chevronnés, puissent découvrir, explorer, s’amuser.</p>
                     <a class="btn btn-primary" href="{{ route('team') }}">Découvrir</a>
                 </div>
             </div>
